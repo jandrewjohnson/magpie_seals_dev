8914e9a2da983d7f2d85fc5823167b4476b723cf
Justin Johnson <jajohns@umn.edu>
2024-06-12 09:01:53 -0500

    third

--- magpie_seals/magpie_integration.py
+++ magpie_seals/magpie_integration.py
@@ -7,9 +7,13 @@ def launch_magpie(p):
     if p.run_this:
         r_repo_dir = '../../../magpie/magpie_develop'
         start_script_filename = 'start.R'
+        
+        direct_path = "../../../magpie/magpie_develop/scripts/start/default.R"
         r_script_path = os.path.join(r_repo_dir, start_script_filename)
         
         # Call r_script_path via subprocess
-        hb.path_exists(r_script_path, verbose=True)
-        subprocess.run(['Rscript', r_script_path]) 
+        hb.path_exists(direct_path, verbose=True)
+        # hb.path_exists(r_script_path, verbose=True)
+        subprocess.run(['Rscript', direct_path]) 
+        # subprocess.run(['Rscript', r_script_path]) 
         
